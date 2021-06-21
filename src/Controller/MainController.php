88de2177493d045a9d86405ad94debab9d9cee0e
alexandredieu <alex.dieudonne64@gmail.com>
2021-06-21 13:12:27 +0200

    save

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,10 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
@@ -26,11 +32,34 @@ class MainController extends AbstractController
     /**
      * @Route("/blog/{id}", name="show")
      */
-    public function show(Article $article)
+    public function show(Article $article, Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment;
+        $form = $this->createForm(CommentType::class, $comment);
+        // $user = $userRepository->find(13);
+        $user = $this->getUser();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() AND $form->isValid()){
+            $comment = $form->getData();
+            $comment->setCreatedAt(new DateTime());
+            $comment->setArticle($article);
+            $comment->setUser($user);
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('show', ['id'  => $article->getId()]);
+        }
+        
         return $this->render('main/_show.html.twig', [
             'article' => $article,
-
+            'form' => $form->createView()
         ]);
+    }
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render("admin.html.twig");
     }
 }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,53 +6,79 @@ use Faker;
 use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\Category;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ArticleFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoderInterface)
+    {
+        $this->encoder = $userPasswordEncoderInterface;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
 
+        $user = new User();
+        $user->setEmail("toto@toto.fr")
+            ->setRoles(['ROLE_USER']);
+        $hash = $this->encoder->encodePassword($user, "toto");
+        $user->setPassword($hash);
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail("admin@admin.fr");
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hash = $this->encoder->encodePassword($admin, "admin");
+        $admin->setPassword($hash);
+        $manager->persist($admin);
+
         //créer 3 catégories fakées
-        for($i =1; $i <= 3; $i++) {
+        for ($i = 1; $i <= 3; $i++) {
             $category = new Category;
             $category->setName($faker->sentence());
-
             $manager->persist($category);
-            
+
+
+
+
             //créer entre 4 et 6 articles
-            for($j = 1; $j <= mt_rand(4, 6); $j++){
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
                 $article = new Article();
 
                 $content = join($faker->paragraphs(5));
 
                 $article->setTitle($faker->sentence())
-                        ->setContent($content)
-                        ->setStatus(true)
-                        ->setImage($faker->imageUrl())
-                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
-                        ->setCategory($category);
+                    ->setContent($content)
+                    ->setStatus(true)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
                 $manager->persist($article);
 
                 //on donne des commentaires à l'article
-                // for($k =1; $k <= mt_rand (4, 10); $k++) {
-                //     $comment = new Comment();
+                for ($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
 
-                //     $content = join($faker->paragraphs(2));
+                    $content = join($faker->paragraphs(2));
 
-                //     $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
 
-                //     $comment->setContent($content)
-                //             ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
-                //             ->setArticle($article);
+                    $comment->setUser($user)
+                        ->setContent($content)
+                        ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
+                        ->setArticle($article);
 
-                //             $manager->persist($comment);
-                // }
+                    $manager->persist($comment);
+                }
             }
         }
-        
+
 
         $manager->flush();
     }
